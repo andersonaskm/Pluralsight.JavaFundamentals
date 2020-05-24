afae88da150786db54af4a511b2bfb89cbc03ae3
Anderson Szalai <it.andersonszalai@conectcar.com>
2020-05-24 12:22:44 -0300

    Java docs on main class and Log4J.

--- src/com/askm/estudo/pluralsight/Main.java
+++ src/com/askm/estudo/pluralsight/Main.java
@@ -1,10 +1,20 @@
 package com.askm.estudo.pluralsight;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+
+    /**
+     * Main entry point for the program.
+     * @param args program entry point arguments
+     */
     public static void main(String[] args) {
 
-        System.out.println("Pluralsight Java Fundamentals.");
+        Logger logger = Logger.getLogger(com.askm.estudo.pluralsight.Main.class);
+
+        String programName = "Pluralsight Java Fundamentals.";
+        logger.debug(programName);
 
     }
 }
