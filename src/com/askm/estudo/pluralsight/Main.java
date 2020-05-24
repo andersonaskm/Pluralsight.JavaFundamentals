4b077139891b2713fb6be169016f4ec807822de8
Anderson Szalai <it.andersonszalai@conectcar.com>
2020-05-24 13:29:15 -0300

    Deleted dummy code.

--- src/com/askm/estudo/pluralsight/Main.java
+++ src/com/askm/estudo/pluralsight/Main.java
@@ -14,35 +14,7 @@ public class Main {
         String programName = "Pluralsight Java Fundamentals.";
         logger.debug(programName);
 
-        testNumbersTypes();
-        testCharBoolTypes();
-
     }
 
-    private static void testNumbersTypes() {
-        byte byteNumber= 120;
-        logger.info(byteNumber);
 
-        short shortNumber = 32000;
-        logger.info(shortNumber);
-
-        int intNumber = 21334434;
-        logger.info(intNumber);
-
-        long longNumber = 45354534L;
-        logger.info(longNumber);
-
-        float floatNumber = 10.4f;
-        logger.info(floatNumber);
-
-        double doubleNumber = 23.45d;
-        logger.info(doubleNumber);
-    }
-
-    private static void testCharBoolTypes() {
-        char singleChar = '\u00DA';
-        logger.info(singleChar);
-
-        logger.info(false);
-    }
 }
