a5dd254f87cfdfcde916f2e7339f89e27dbfe7d0
Anderson Szalai <it.andersonszalai@conectcar.com>
2020-05-24 13:10:50 -0300

    Java primitive data types.

--- src/com/askm/estudo/pluralsight/Main.java
+++ src/com/askm/estudo/pluralsight/Main.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 public class Main {
 
-    private static Logger logger = Logger.getRootLogger();
+    private static final Logger logger = Logger.getRootLogger();
     /**
      * Main entry point for the program.
      * @param args program entry point arguments
@@ -14,5 +14,35 @@ public class Main {
         String programName = "Pluralsight Java Fundamentals.";
         logger.debug(programName);
 
+        testNumbersTypes();
+        testCharBoolTypes();
+
+    }
+
+    private static void testNumbersTypes() {
+        byte byteNumber= 120;
+        logger.info(byteNumber);
+
+        short shortNumber = 32000;
+        logger.info(shortNumber);
+
+        int intNumber = 21334434;
+        logger.info(intNumber);
+
+        long longNumber = 45354534L;
+        logger.info(longNumber);
+
+        float floatNumber = 10.4f;
+        logger.info(floatNumber);
+
+        double doubleNumber = 23.45d;
+        logger.info(doubleNumber);
+    }
+
+    private static void testCharBoolTypes() {
+        char singleChar = '\u00DA';
+        logger.info(singleChar);
+
+        logger.info(false);
     }
 }
